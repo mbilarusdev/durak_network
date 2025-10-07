3e0afe96c962252ab9d42ef39612c096da0937f6
mbilarusdev <maxim.bilarus@gmail.com>
2025-10-07 15:12:19 +0300

    handler param type changed

--- network/handler.go
+++ network/handler.go
@@ -3,7 +3,7 @@ package network
 import "net/http"
 
 func Handler(
-	inner func(w http.ResponseWriter, r *http.Request) *DurakResponse[any],
+	inner func(w http.ResponseWriter, r *http.Request) *DurakHandlerResult,
 ) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		inner(w, r)

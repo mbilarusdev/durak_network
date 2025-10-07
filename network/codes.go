4ecef26cfec0df193d51ef56ad0930fd4100e026
mbilarusdev <maxim.bilarus@gmail.com>
2025-10-07 15:15:46 +0300

    change read body and unmarshaling error codes

--- network/codes.go
+++ network/codes.go
@@ -2,13 +2,13 @@ package network
 
 const (
 	// Errors
-	CodeServerError       int = 1
-	CodeUnmarshalingError int = 2
+	CodeServerError   int = 1
+	CodeReadBodyError int = 2
 
 	// Client warnings
-	CodeIncorrectBody int = 11
-	CodeAlreadyExist  int = 12
-	CodeNotFound      int = 13
+	CodeUnmarshalingError int = 11
+	CodeAlreadyExist      int = 12
+	CodeNotFound          int = 13
 
 	// Token errors
 	CodeTokenNotProvided int = 21
